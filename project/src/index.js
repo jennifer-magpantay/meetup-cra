c37e353a4584f2393ac1a2d24a7d3840330c1aef
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-08-23 17:32:22 +0200

    Add routing to index

--- project/src/index.js
+++ project/src/index.js
@@ -1,13 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./styles/app.scss";
 
 import { App } from "./pages/App";
+import { Meetups } from "./pages/Meetups";
+import { Favorites } from "./pages/Favorites";
+import { NewMeetup } from "./pages/NewMeetup";
+import { Error } from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/meetups",
+    element: <Meetups />,
+  },
+  {
+    path: "/favourites",
+    element: <Favorites />,
+  },
+  {
+    path: "/new-meetup",
+    element: <NewMeetup />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
