27a72cbf84d677637e12803bf770c90fc9a95d97
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-08-24 11:45:39 +0200

    Add data-count to favourites link

--- project/src/components/Navigation.jsx
+++ project/src/components/Navigation.jsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { FavouritesContext } from "../store/context";
 
 const links = [
   { id: 1, url: "/", label: "Home" },
   { id: 2, url: "/meetups", label: "Meetups" },
-  { id: 3, url: "/favourites", label: "Favourites" },
-  { id: 4, url: "/new-meetup", label: "New Meetup" },
+  { id: 3, url: "/new-meetup", label: "New Meetup" },
 ];
 
 export const Navigation = () => {
+  const context = useContext(FavouritesContext);
+  const hasFavourites = context.favourites.length !== 0;
+  const countClass = [
+    "navigation--list-link",
+    hasFavourites ? "count" : "",
+  ].join(" ");
+
   return (
     <nav className="navigation">
       <ul className="navigation--list">
@@ -16,6 +24,9 @@ export const Navigation = () => {
             <Link to={url}>{label}</Link>
           </li>
         ))}
+        <li className={countClass} data-count={context.totalFavourites}>
+          <Link to="/favourites">Favourites</Link>
+        </li>
       </ul>
     </nav>
   );
