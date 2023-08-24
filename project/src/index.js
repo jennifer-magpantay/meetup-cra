98f5f02c33bf4a6319e7181fd3b17860b0871112
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-08-24 11:46:16 +0200

    Update index.js

--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { FavouritesContextProvider } from "./store/context";
 
 import "./styles/app.scss";
 
@@ -33,6 +34,8 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <FavouritesContextProvider>
+      <RouterProvider router={router} />
+    </FavouritesContextProvider>
   </React.StrictMode>
 );
